ef7217c574b72702296e773e5d0f5454062b9202
Ethan <nicebuvr@naver.com>
2021-03-08 02:37:05 +0900

    Update LoginView UI

---
+++ BAEPPO/BAEPPO/Login/ForgotPasswordViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ForgotPasswordViewController.swift
+//  BAEPPO
+//
+//  Created by Ethan on 2021/03/08.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+    
+}
--- BAEPPO/BAEPPO/Login/LoginViewController.swift
+++ BAEPPO/BAEPPO/Login/LoginViewController.swift
@@ -11,6 +11,33 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.setupViews()
+    }
+
+    private func setupViews() {
+        self.setupTextField()
+    }
+
+    private func setupTextField() {
+        self.idTextField.delegate = self
+        self.passwordTextField.delegate = self
+    }
+
+    @IBAction private func didTapLoginButton(_ sender: UIButton) {
+    }
+
+    @IBOutlet private weak var idTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { true }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool { true }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
     }
 
 }
